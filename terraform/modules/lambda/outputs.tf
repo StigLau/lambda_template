183db17622b1cdf12a7fd06a532339615f18bb27
stiglau <stig@lau.no>
2023-05-16 14:27:11 +0200

    Cleaning up TF scripts

--- terraform/modules/lambda/outputs.tf
+++ terraform/modules/lambda/outputs.tf
@@ -1,4 +1,4 @@
-output "lambda_arn" {
+output lambda_arn {
   description = "Full ARN of the Task Definition (including both family and revision)."
   value       = aws_lambda_function.lambda_template_function.arn
 }
